45457fe82ebe017a36322a56c84939fac82edf3e
Alexander Schmidt <alexanderschmidt@sprintmac.speedport.ip>
2020-03-25 18:26:13 +0100

    added script for osx build

--- Ui/Ui.ConsoleApp/publish-osx.sh
+++ Ui/Ui.ConsoleApp/publish-osx.sh
@@ -1,2 +1,3 @@
 dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
-openssl dgst -sha256 bin/Release/netcoreapp3.1/osx-x64/publish/pping
+tar -czf bin/Release/netcoreapp3.1/osx-x64/publish/pping-osx-latest.tgz bin/Release/netcoreapp3.1/osx-x64/publish/pping
+openssl dgst -sha256 bin/Release/netcoreapp3.1/osx-x64/publish/pping-osx-latest.tgz
