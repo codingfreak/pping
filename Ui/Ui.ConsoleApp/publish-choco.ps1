3191f0ca47cffb0955b86424d67571fb904662b8
devdeer-alex <alexander.schmidt@devdeer.com>
2020-11-30 15:17:44 +0100

    Moved to .NET 5

--- Ui/Ui.ConsoleApp/publish-choco.ps1
+++ Ui/Ui.ConsoleApp/publish-choco.ps1
@@ -1,12 +1,12 @@
-$folder = ".\bin\Release\netcoreapp3.1\publish"
+$folder = ".\bin\Release\net5.0\win-x64\publish\"
 # Execute the dotnet publish command for self-contained EXE and Windows
-dotnet publish -c Release
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 # Calculate the new Hash:
 $hashExe = Get-FileHash $folder\pping.exe | Select -ExpandProperty Hash
-$hashDll = Get-FileHash $folder\pping.dll | Select -ExpandProperty Hash
+#$hashDll = Get-FileHash $folder\pping.dll | Select -ExpandProperty Hash
 # Replace hash in verification.txt
 (Get-Content verification.txt) -replace '- pping.exe \(SHA256: (.*)', "- pping.exe (SHA256: $hashExe)" | Out-File verification.txt
-(Get-Content verification.txt) -replace '- pping.dll \(SHA256: (.*)', "- pping.dll (SHA256: $hashDll)" | Out-File verification.txt
+#(Get-Content verification.txt) -replace '- pping.dll \(SHA256: (.*)', "- pping.dll (SHA256: $hashDll)" | Out-File verification.txt
 # copy files to publish-folder for packing
 cp pping.nuspec $folder\pping.nuspec
 cp verification.txt $folder\verification.txt
