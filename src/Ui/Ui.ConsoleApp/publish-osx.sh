3e95d9dc96e390f57f79c27ffe4338126604a47a
codingfreak <sprinter@codingfreaks.de>
2026-09-10 12:41:04 +0200

    Added publish script

--- src/Ui/Ui.ConsoleApp/publish-osx.sh
+++
@@ -1,3 +0,0 @@
-﻿dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
-tar -czf bin/Release/netcoreapp3.1/osx-x64/publish/pping-osx-latest.tgz bin/Release/net10.0/osx-x64/publish/pping
-openssl dgst -sha256 bin/Release/net10.0/osx-x64/publish/pping-osx-latest.tgz
